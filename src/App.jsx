50d8b0899be12fd0edbb7503884e02a3d7f017c8
Taro <rout0052@algonquinlive.com>
2026-07-15 14:50:08 -0400

    Adds GameOverScreen, highscores, scrambledSpell value, and fixes the issue of moving to the next spell

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import Counter from './components/Counter.jsx';
 import GameOverScreen from './components/GameOverScreen.jsx';
 
 function App() {
-  let gameOver = false;
+  const [gameOver, setGameOver] = useState(false);
 
   const [strikes, setStrikes] = useState(() =>
-    parseInt(JSON.parse(localStorage.getItem('strikes'))) || 10);
+    parseInt(JSON.parse(localStorage.getItem('strikes'))) || 0);
 
   const [passes, setPasses] = useState(() =>
     parseInt(JSON.parse(localStorage.getItem('passes'))) || 10);
@@ -16,11 +16,17 @@ function App() {
   const [points, setPoints] = useState(() =>
     parseInt(JSON.parse(localStorage.getItem('points'))) || 0);
 
-  const [spellsArray, setSpellsArray] = useState(() => JSON.parse(localStorage.getItem('spells')) || shuffle(spells))
+  const [highScore, setHighScore] = useState(() => parseInt(JSON.parse(localStorage.getItem('highscore'))) || 0)
 
-  const [currentSpell, setCurrentSpell] = spellsArray[0];
+  const [spellsArray, setSpellsArray] = useState(() => shuffle(JSON.parse(localStorage.getItem('spells'))) || shuffle(spells))
+
+  const [currentSpell, setCurrentSpell] = useState(spellsArray[0]);
+  const [scrambledSpell, setScrambledSpell] = useState(shuffle(currentSpell));
+
+  const [userGuess, setUserGuess] = useState('');
 
   const passDisabledState = (passes == 0) ? true : false;
+  const [isNewHighScore, setIsNewHighScore] = useState(false);
 
   useEffect(() => {
     localStorage.setItem('passes', JSON.stringify(passes))
@@ -28,58 +34,84 @@ function App() {
 
   useEffect(() => {
     localStorage.setItem('strikes', JSON.stringify(strikes));
-    if(strikes >= 10) {
-      gameOver = true;
+    if (strikes >= 10) {
+      setGameOver(true);
     }
   }, [strikes]);
 
   useEffect(() => {
     localStorage.setItem('points', JSON.stringify(points))
+    if (points > highScore) {
+      setHighScore(points);
+      setIsNewHighScore(true);
+    }
   }, [points]);
 
   useEffect(() => {
-    // setCurrentSpell(spellsArray[0]);
+    localStorage.setItem('highscore', JSON.stringify(highScore))
+  }, [highScore]);
+
+  useEffect(() => {
+    localStorage.setItem('spells', JSON.stringify(spellsArray))
+    setCurrentSpell(spellsArray[0])
   }, [spellsArray]);
 
   useEffect(() => {
-    console.log('The game is now over.')
-  }, [gameOver]);
+    setScrambledSpell(shuffle(currentSpell));
+  }, [currentSpell]);
 
+  const nextSpell = () => {
+    if (spellsArray.length > 1) {
+      setSpellsArray(spellsArray.slice(1))
+    } else {
+      setGameOver(true);
+    }
+
+    setUserGuess('')
+  }
+
+  const resetGame = () => {
+    setGameOver(false);
+    setUserGuess('')
+    setSpellsArray(shuffle(spells));
+    setStrikes(0);
+    setPasses(10);
+    setPoints(0);
+  }
 
   // setPasses(5)
   // localStorage.setItem('passes', JSON.stringify(5))
+  // setStrikes(0);
+  // localStorage.setItem('strikes', JSON.stringify(0))
+  // setSpellsArray(spells)
+  // localStorage.setItem('spells', JSON.stringify(spellsArray))
+
 
   const passHandler = () => {
     setPasses(passes - 1)
     if (passes <= 0) {
       passDisabledState = true;
-      console.log(passDisabledState)
     }
+
+    nextSpell();
   }
 
   const submitHandler = (e) => {
     e.preventDefault();
-    // console.log(e.target.firstChild.value);
-    // console.log(spellsArray[0])
-    // compare(e.target.firstChild.value.toLowerCase(), spellsArray[0])
-    if(compare(e.target.firstChild.value.toLowerCase(), spellsArray[0])) {
-      console.log(true);
+    if (compare(e.target.firstChild.value.toLowerCase(), currentSpell)) {
       setPoints(points + 1);
-
+      alert(`Correct! The spell was in fact '${currentSpell}'`);
     } else {
-      console.log(false);
       setStrikes(strikes + 1);
-      console.log(strikes);
+      alert(`Wrong :( The spell was '${currentSpell}'`);
     }
 
-    console.log(spellsArray);
-    setSpellsArray(spellsArray.shift());
-    console.log(spellsArray);
-    console.log(shuffle(currentSpell.toUpperCase()))
+    nextSpell();
+
+    // Easy Mode: Do you want to test in a way that's easier because some of these are kinda super hard? Uncomment the next 2 lines, and you can see the next correct answer
+    // console.log(currentSpell);
+    // console.log(spellsArray);
   }
-
-
-
 
   return (
     <div className='column is-three-fifths mx-auto my-4'>
@@ -109,10 +141,10 @@ function App() {
           <Counter propertyName='Strikes' currentCount={strikes} />
         </div>
 
-        <p className='is-size-1 has-text-weight-bold has-text-centered'>{shuffle(currentSpell.toUpperCase())}</p>
+        <p className='is-size-1 has-text-weight-bold has-text-centered'>{scrambledSpell.toUpperCase()}</p>
 
         <form onSubmit={submitHandler} className='column is-three-fifths mx-auto'>
-          <input type="text" name="spellGuess" id="spellGuess" className='input is-info my-2' />
+          <input type="text" name="spellGuess" id="spellGuess" value={userGuess} onChange={(e) => setUserGuess(e.target.value)} className='input is-info my-2' />
           <div className='buttons is-justify-content-center are-medium my-2'>
             <button onClick={passHandler} type="button" className='button is-danger' disabled={passDisabledState}>Pass</button>
 
@@ -120,11 +152,35 @@ function App() {
           </div>
 
         </form>
-
-
+        
+        <p className='is-size-3 has-text-weight-medium has-text-centered'>Highscore: {highScore}</p>
 
 
       </main>
+
+      
+
+      { /* Conditionally rendering the GameOverScreen so it does not update constantly.*/ 
+      
+      gameOver ?
+        <GameOverScreen
+          modalStatus={gameOver}
+
+          resetHandler={resetGame}
+
+          highScore={{ value: highScore, new: isNewHighScore }}
+
+          playerStats={[
+            { name: 'Total Points', value: points, key: crypto.randomUUID() },
+            { name: 'Passes Remaining', value: passes, key: crypto.randomUUID()},
+            { name: 'Total Strikes', value: strikes, key: crypto.randomUUID() }
+          ]}
+        />
+        : ''}
+
+        {/* 'Kill switch' that you can uncomment for testing to reset the game :) */}
+
+        {/* <button onClick={resetGame} className="button is-small is-danger">Start again</button>   */}
 
     </div>
   )
